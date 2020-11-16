7b36b2a6d068409e25032cf6bee83d358f65f148
Ilarion Soloviov <larez232323@gmail.com>
2020-11-16 18:00:50 +0200

    HW16.1:git reflog

--- HW15_Git/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.bar_mobile').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-})
